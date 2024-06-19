a141b06509a28faa391ae74281935e5fae3c40a4
Eric Brown <ericwb@users.noreply.github.com>
2024-06-19 15:09:06 -0700

    Update build script to do checks and install (#2)
    Signed-off-by: Eric Brown <eric_wade_brown@yahoo.com>

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
-echo "TODO implement this script."
+if ! command -v python3.12 &>/dev/null; then
+  echo "Python 3.12 is not installed and required for this extension."
+  exit 1
+fi
+
+if ! python3.12 -m pip --version &>/dev/null; then
+  echo "Pip is not installed and required for this extension."
+  exit 1
+fi
+
+#python3.12 -m pip install --ignore-installed --target=dist precli
+
 echo "It should build binaries in dist/<platform>-<arch>[.exe] as needed."
-exit 1
